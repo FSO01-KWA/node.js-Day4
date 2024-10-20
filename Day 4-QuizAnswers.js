7c67c2eac9c1b44b1129b1d3a123746cb0da9d3c
FSO01-KWA <keylakim22@gmail.com>
2024-10-21 08:20:40 +0900

    퀴즈 정답 파일 추가

---
+++ Day 4-QuizAnswers.js
@@ -0,0 +1,15 @@
+const quizAnswers = {
+    1: 'b',
+    2: 'd',
+    3: 'b',
+    4: 'd',
+    5: 'a',
+    6: 'c',
+    7: 'b',
+    8: 'c',
+    9: 'c',
+    10: 'c',
+  };
+  
+  module.exports = quizAnswers;
+  
